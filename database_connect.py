81cfd1de09160f120ce4b47bcbe28532126d484c
vijay shankar <vijayshankar@vijays-MacBook-Pro.local>
2025-02-19 17:06:33 +0530

    added postgress

--- database_connect.py
+++ database_connect.py
@@ -1,6 +1,20 @@
 import databases as dbs
 import sqlalchemy as sa
-DATABASE_URL = "postgresql://postgres:123@localhost/curve"
+import os
+
+POSTGRES_USER = os.environ.get("username", "postgres")
+POSTGRES_PASSWORD = os.environ.get("password", "secret")
+POSTGRES_DB = os.environ.get("database", "mydatabase")
+POSTGRES_HOST = os.environ.get("hostname", "localhost")
+POSTGRES_PORT = os.environ.get("port", "5432")
+
+# Construct the connection URL
+DATABASE_URL = (
+    f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}"
+    f"@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+)
+
+print("Connection URL:", DATABASE_URL)
 
 dbs = dbs.Database(DATABASE_URL)
 metadata = sa.MetaData()
